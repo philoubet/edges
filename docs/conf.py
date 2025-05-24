7155204ab2947528417782c539f82ab4f451d900
romainsacchi <r_s@me.com>
2025-05-24 13:23:06 +0200

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,3 +13,8 @@ exclude_patterns = []
 
 html_theme = "alabaster"
 html_static_path = ["_static"]
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))  # or '../src' if your code is in src/
+
